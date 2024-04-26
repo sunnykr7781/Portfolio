ffadb5cb1563afbabca9925240f4e761656617bf
sunny <sunny129271@gmail.com>
2024-04-26 07:09:26 +0530

    added skills section

---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,170 @@
+import React from "react"
+import "./Skills.css"
+
+const Skills = () => {
+  return (
+    <>
+      <div id="skills">
+        <div className="heading">
+          <h2>explore my</h2>
+          <h1>skills</h1>
+        </div>
+        <div className="main">
+          <div className="parent">
+            <div className="child1">
+              <div className="nbar">
+                <h1>frontend development</h1>
+              </div>
+
+              <div className="row">
+                <div className="one">
+                  <i class="ri-checkbox-circle-fill"></i>
+                  <h4>html</h4>
+                </div>
+                <div className="two">
+                  <i class="ri-checkbox-circle-fill"></i>
+                  <h4>
+                    css&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    &nbsp;
+                  </h4>
+                </div>
+              </div>
+              <div className="row">
+                <div className="one">
+                  <i class="ri-checkbox-circle-fill"></i>
+                  <h4>tailwind</h4>
+                </div>
+                <div className="two">
+                  <i class="ri-checkbox-circle-fill"></i>
+                  <h4>bootstrap&nbsp;</h4>
+                </div>
+              </div>
+              <div className="row">
+                <div className="one">
+                  <i class="ri-checkbox-circle-fill"></i>
+                  <h4>React js</h4>
+                </div>
+                <div className="two">
+                  <i class="ri-checkbox-circle-fill"></i>
+                  <h4>javascript</h4>
+                </div>
+              </div>
+            </div>
+            <div className="child1">
+              <div className="nbar">
+                <h1>backend development</h1>
+              </div>
+
+              <div className="row">
+                <div className="one">
+                  <i class="ri-checkbox-circle-fill"></i>
+                  <h4>node js</h4>
+                </div>
+                <div className="two">
+                  <i class="ri-checkbox-circle-fill"></i>
+                  <h4>express js</h4>
+                </div>
+              </div>
+              <div className="row">
+                <div className="one">
+                  <i class="ri-checkbox-circle-fill"></i>
+                  <h4>restful api</h4>
+                </div>
+                <div className="two">
+                  <i class="ri-checkbox-circle-fill"></i>
+                  <h4>mongo db</h4>
+                </div>
+              </div>
+              <div className="row">
+                <div className="one">
+                  <i class="ri-checkbox-circle-fill"></i>
+                  <h4>firebase</h4>
+                </div>
+                <div className="two">
+                  <i class="ri-checkbox-circle-fill"></i>
+                  <h4>crypto js&nbsp;&nbsp;</h4>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="parent">
+            <div className="child1">
+              <div className="nbar">
+                <h1>core subjects</h1>
+              </div>
+
+              <div className="row">
+                <div className="one">
+                  <i class="ri-checkbox-circle-fill"></i>
+                  <h4>dsa</h4>
+                </div>
+                <div className="two">
+                  <i class="ri-checkbox-circle-fill"></i>
+                  <h4>java&nbsp;&nbsp;&nbsp;</h4>
+                </div>
+              </div>
+              <div className="row">
+                <div className="one">
+                  <i class="ri-checkbox-circle-fill"></i>
+                  <h4>computer networks</h4>
+                </div>
+                <div className="two">
+                  <i class="ri-checkbox-circle-fill"></i>
+                  <h4>oops&nbsp;&nbsp;</h4>
+                </div>
+              </div>
+              <div className="row">
+                <div className="one">
+                  <i class="ri-checkbox-circle-fill"></i>
+                  <h4>operating system</h4>
+                </div>
+                <div className="two">
+                  <i class="ri-checkbox-circle-fill"></i>
+                  <h4>dbms&nbsp;</h4>
+                </div>
+              </div>
+            </div>
+            <div className="child1">
+              <div className="nbar">
+                <h1>others</h1>
+              </div>
+
+              <div className="row">
+                <div className="one">
+                  <i class="ri-checkbox-circle-fill"></i>
+                  <h4>post man</h4>
+                </div>
+                <div className="two">
+                  <i class="ri-checkbox-circle-fill"></i>
+                  <h4>mongoose</h4>
+                </div>
+              </div>
+              <div className="row">
+                <div className="one">
+                  <i class="ri-checkbox-circle-fill"></i>
+                  <h4>git/github</h4>
+                </div>
+                <div className="two">
+                  <i class="ri-checkbox-circle-fill"></i>
+                  <h4>vs code&nbsp;&nbsp;&nbsp;&nbsp;</h4>
+                </div>
+              </div>
+              <div className="row">
+                <div className="one">
+                  <i class="ri-checkbox-circle-fill"></i>
+                  <h4>sass/scss</h4>
+                </div>
+                <div className="two">
+                  <i class="ri-checkbox-circle-fill"></i>
+                  <h4>redux &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h4>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Skills
